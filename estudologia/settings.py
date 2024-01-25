620c04fa9f58724aaabb8c9e179e7ee69a456bd7
goiabaverde <goiabinhafn@gmail.com>
2024-01-25 08:44:52 -0300

    some changes in settings.py

--- estudologia/settings.py
+++ estudologia/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5_$w4^&w3-8^o6&a86c%&c-g)te=l@)7_dgo073g-)pwwr!l0-'
-
+with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
+    SECRET_KEY = f.read().strip()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
